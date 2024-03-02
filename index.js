7b3cf74db6dc00fe10597c60dfd593e6831ac602
pramaths <103276292+pramaths@users.noreply.github.com>
2024-03-02 11:53:29 +0530

    default

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// const express = require("express");
+const express = require("express");
 // const bodyParser = require("body-parser");
 // const cookieParser = require("cookie-parser");
 // // const socketIo = require("socket.io");
 // // const morgan = require("morgan");
-// const cors = require("cors");
+ const cors = require("cors");
 // require("dotenv").config();
 
 // // const http = require("http");
-// const app = express();
+ const app = express();
 // const port = 8000;
 // // const server = http.createServer(app);
 // // const io = socketIo(server);
 // // const EthtransactionRoutes = require("./routes/Ethereum");
 // // const BitcointransactionsRouter = require("./routes/Bitcoin");
-// app.use(cors());
+app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 // app.use(cookieParser());
@@ -25,28 +25,14 @@
 // //   console.error(err.stack);
 // //   res.status(500).send("Something broke!");
 // // });
-// app.get("/", (req, res) => {
-//   res.status(200).send("Welcome to the Crypto Tracker API");
-// });
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the Crypto Tracker API");
+});
 
 // // app.use("/api", BitcointransactionsRouter);
 // // app.use("/api", EthtransactionRoutes);
 
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-
-const express  = require("express")
-const app = express()
-
-const cors = require("cors")
-app.use(cors())
-
-app.get("/", (req, res) => {
-    res.status(200).json({"Name": "Hi"})
-})
-app.listen(3000, () => {
-    console.log("http://localhost:3000")
-})
+app.listen(8000, () => {
+  console.log(`Server is running on port`);
+});
