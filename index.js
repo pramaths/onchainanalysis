6ea76a6e7382744d24bcbe9075c750f9280a1e5a
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2024-03-02 22:16:24 +0530

    input search

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const socketIo = require("socket.io");
 const morgan = require("morgan");
- const cors = require("cors");
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "testaccout33@gmail.com",
+    pass: "rexmriznhrrkegcc",
+  },
+});
+
+const cors = require("cors");
 require("dotenv").config();
 
 const http = require("http");
- const app = express();
+const app = express();
 const port = 8000;
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -29,7 +40,30 @@ app.get("/", (req, res) => {
 
 app.use("/api", BitcointransactionsRouter);
 app.use("/api", EthtransactionRoutes);
+app.post("/snooping-account", (req, res) => {
+  const mailOptions = {
+    to: "namkarthik2003@gmail.com", //The Embrione Mail comes here.
+    subject: `Suspicious activity on tagged account`,
+    text: JSON.stringify(req.body),
+  };
 
+  transporter.sendMail(mailOptions, (error, info) => {
+    // this is to bypass all the mails via the embrione tech mail as well
+    if (error) {
+      console.log(error);
+      res.status(200).json({
+        message: JSON.stringify(error.message),
+      });
+    } else {
+      // console.log("Email sent: " + info.response + Date.now());
+      console.log("Mail Sent Successfully!");
+      res.status(200).json({
+        message: "Success! Message Sent! ",
+      });
+    }
+    // res.json({name: 'This is the backendddd'})
+  });
+});
 
 app.listen(8000, () => {
   console.log(`Server is running on port`);
