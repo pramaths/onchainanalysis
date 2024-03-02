da676bc3e64beeca4fd093bd8c4847e2c6f217ce
pramaths <103276292+pramaths@users.noreply.github.com>
2024-03-02 12:34:02 +0530

    hosting

--- services/blockchain/Ethereum/transaction.js
+++ services/blockchain/Ethereum/transaction.js
@@ -1,13 +1,11 @@
 const Moralis = require('moralis').default;
 
-const initializeMoralis = async () => {
-  await Moralis.start({ apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImEzZTA5ZDgyLTU3YjEtNGZlMy04MjY3LTk3NWI1MjZmOWJmZCIsIm9yZ0lkIjoiMjYxNjMzIiwidXNlcklkIjoiMjY1ODk2IiwidHlwZUlkIjoiMjU5MTRlZGYtNDdjMy00Y2ZhLWI1NjktMGMzNDg3ZWQ1NTI3IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDYxODExOTIsImV4cCI6NDg2MTk0MTE5Mn0._EvFooXZJKB4aRbKXf_W6-VJJv9S_IaYBZUZOyC0Jtg'});
-};
-
+Moralis.start({
+  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImEzZTA5ZDgyLTU3YjEtNGZlMy04MjY3LTk3NWI1MjZmOWJmZCIsIm9yZ0lkIjoiMjYxNjMzIiwidXNlcklkIjoiMjY1ODk2IiwidHlwZUlkIjoiMjU5MTRlZGYtNDdjMy00Y2ZhLWI1NjktMGMzNDg3ZWQ1NTI3IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDYxODExOTIsImV4cCI6NDg2MTk0MTE5Mn0._EvFooXZJKB4aRbKXf_W6-VJJv9S_IaYBZUZOyC0Jtg'
+}).then(() => console.log('Moralis initialized successfully.'))
+  .catch((error) => console.error('Failed to initialize Moralis:', error));
 const getWalletTransactions = async (req, res) => {
   try {
-    await initializeMoralis(); 
-
     const { chain, address } = req.params; 
     console.log(chain,address)
     const response = await Moralis.EvmApi.transaction.getWalletTransactions({
