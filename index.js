36dcf38424c814ca500ebc5db862a0bfdb5fa4dc
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2024-03-02 11:23:23 +0530

    monr fixes

--- index.js
+++ index.js
@@ -18,17 +18,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use("/api", BitcointransactionsRouter);
-app.use("/api", EthtransactionRoutes);
+
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send("Something broke!");
+// });
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to the Crypto Tracker API");
 });
+
+app.use("/api", BitcointransactionsRouter);
+app.use("/api", EthtransactionRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
