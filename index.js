75c8be73b34557425d8874f8a2e2149f0bdcb1aa
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2024-03-02 11:28:17 +0530

    monr fixes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const socketIo = require("socket.io");
+// const socketIo = require("socket.io");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const http = require("http");
+// const http = require("http");
 const app = express();
 const port = 8000;
-const server = http.createServer(app);
-const io = socketIo(server);
-const EthtransactionRoutes = require("./routes/Ethereum");
-const BitcointransactionsRouter = require("./routes/Bitcoin");
+// const server = http.createServer(app);
+// const io = socketIo(server);
+// const EthtransactionRoutes = require("./routes/Ethereum");
+// const BitcointransactionsRouter = require("./routes/Bitcoin");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -29,8 +29,8 @@ app.get("/", (req, res) => {
   res.status(200).send("Welcome to the Crypto Tracker API");
 });
 
-app.use("/api", BitcointransactionsRouter);
-app.use("/api", EthtransactionRoutes);
+// app.use("/api", BitcointransactionsRouter);
+// app.use("/api", EthtransactionRoutes);
 
 
 app.listen(port, () => {
