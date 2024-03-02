7082d556662412bca4442370bb13a3cee18a55ce
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2024-03-02 11:36:23 +0530

    new

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-// const socketIo = require("socket.io");
-// const morgan = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cookieParser = require("cookie-parser");
+// // const socketIo = require("socket.io");
+// // const morgan = require("morgan");
+// const cors = require("cors");
+// require("dotenv").config();
 
-// const http = require("http");
-const app = express();
-const port = 8000;
-// const server = http.createServer(app);
-// const io = socketIo(server);
-// const EthtransactionRoutes = require("./routes/Ethereum");
-// const BitcointransactionsRouter = require("./routes/Bitcoin");
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-// app.use(morgan("dev"));
+// // const http = require("http");
+// const app = express();
+// const port = 8000;
+// // const server = http.createServer(app);
+// // const io = socketIo(server);
+// // const EthtransactionRoutes = require("./routes/Ethereum");
+// // const BitcointransactionsRouter = require("./routes/Bitcoin");
+// app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+// // app.use(morgan("dev"));
 
 
-// Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Something broke!");
+// // Error handling middleware
+// // app.use((err, req, res, next) => {
+// //   console.error(err.stack);
+// //   res.status(500).send("Something broke!");
+// // });
+// app.get("/", (req, res) => {
+//   res.status(200).send("Welcome to the Crypto Tracker API");
 // });
+
+// // app.use("/api", BitcointransactionsRouter);
+// // app.use("/api", EthtransactionRoutes);
+
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
+const express  = require("express")
+const app = express()
+
+const cors = require("cors")
+app.use(cors())
+
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to the Crypto Tracker API");
-});
-
-// app.use("/api", BitcointransactionsRouter);
-// app.use("/api", EthtransactionRoutes);
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    res.status(200).json({"Name": "Hi"})
+})
+app.listen(3000, () => {
+    console.log("http://localhost:3000")
+})
