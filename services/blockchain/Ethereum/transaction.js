1ff6ece961bf7b41f5b88087229e314c0389a0bf
Larry8668 <114809719+Larry8668@users.noreply.github.com>
2024-06-27 16:12:47 +0530

    redis working (commented untill host)

--- services/blockchain/Ethereum/transaction.js
+++ services/blockchain/Ethereum/transaction.js
@@ -1,4 +1,5 @@
 const Moralis = require("moralis").default;
+const redisClient = require('../../../utils/redis');
 Moralis.start({
   apiKey:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImE0OWZiNmVmLTFlMjEtNDEwOC1hOGEyLTMyZWNhYmIwZDIyYSIsIm9yZ0lkIjoiMzgwOTMyIiwidXNlcklkIjoiMzkxNDI0IiwidHlwZUlkIjoiYmUzNWFhZDgtNjllYi00MGI3LWFlZWYtYzA2NDJiY2ZmNGY0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDk0MDUxNDQsImV4cCI6NDg2NTE2NTE0NH0.X2AL9Ir4Maawo1KiS7A2HxF6eyJewCD80mkRioQdzsA",
@@ -8,18 +9,28 @@ Moralis.start({
 
 const getWalletTransactions = async (req, res) => {
   try {
-    const { chain, address } = req.params;
-    const pagesize = parseInt(req.params.pagesize, 10) || 100;
+    // const cachedData = await redisClient.get("cachedData");
+    // if (!cachedData) {
+      const { chain, address } = req.params;
+      const pagesize = parseInt(req.params.pagesize, 10) || 2;
 
-    console.log(chain, address);
+      console.log(chain, address);
 
-    const response = await Moralis.EvmApi.transaction.getWalletTransactions({
-      chain,
-      address,
-      limit: pagesize || 100,
-      order: "DESC",
-    });
-    res.json(response.raw);
+      const response = await Moralis.EvmApi.transaction.getWalletTransactions({
+        chain,
+        address,
+        limit: pagesize || 2,
+        order: "DESC",
+      });
+      
+      // console.log("Data retrieved from API ->", JSON.parse(response.raw.result.length));
+      // await redisClient.set("cachedData", JSON.stringify(response.raw));
+      // console.log("Caching in Redis...")
+      res.json(response.raw);
+    // } else {
+    //   console.log("Data retrieved from Redis cache ->", JSON.parse(cachedData).result.length);
+    //   res.json(JSON.parse(cachedData));
+    // }
   } catch (e) {
     console.error(e);
     res.status(500).send("An error occurred while fetching transactions");
