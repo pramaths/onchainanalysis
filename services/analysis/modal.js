0dacb43c673b7f7d095be38556c0faf813ea7a9a
pramaths <103276292+pramaths@users.noreply.github.com>
2024-07-01 23:01:58 +0530

    bitcoin transaction

--- services/analysis/modal.js
+++ services/analysis/modal.js
@@ -6,27 +6,27 @@ const alchemy = new Alchemy({
 });
 
 
-const Moralis = require("moralis").default;
-const { EvmChain } = require("@moralisweb3/common-evm-utils");
+// const Moralis = require("moralis").default;
+// const { EvmChain } = require("@moralisweb3/common-evm-utils");
 
-const runApp = async () => {
-//   await Moralis.start({
-//     apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImE0OWZiNmVmLTFlMjEtNDEwOC1hOGEyLTMyZWNhYmIwZDIyYSIsIm9yZ0lkIjoiMzgwOTMyIiwidXNlcklkIjoiMzkxNDI0IiwidHlwZUlkIjoiYmUzNWFhZDgtNjllYi00MGI3LWFlZWYtYzA2NDJiY2ZmNGY0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDk0MDUxNDQsImV4cCI6NDg2NTE2NTE0NH0.X2AL9Ir4Maawo1KiS7A2HxF6eyJewCD80mkRioQdzsA",
+// const runApp = async () => {
+// //   await Moralis.start({
+// //     apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImE0OWZiNmVmLTFlMjEtNDEwOC1hOGEyLTMyZWNhYmIwZDIyYSIsIm9yZ0lkIjoiMzgwOTMyIiwidXNlcklkIjoiMzkxNDI0IiwidHlwZUlkIjoiYmUzNWFhZDgtNjllYi00MGI3LWFlZWYtYzA2NDJiY2ZmNGY0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDk0MDUxNDQsImV4cCI6NDg2NTE2NTE0NH0.X2AL9Ir4Maawo1KiS7A2HxF6eyJewCD80mkRioQdzsA",
+// //   });
+
+//   const address = "0x26fcbd3afebbe28d0a8684f790c48368d21665b5";
+
+//   const chains = [EvmChain.ETHEREUM, EvmChain.BSC, EvmChain.POLYGON , EvmChain.ARBITRUM, EvmChain.AVALANCHE, EvmChain.BASE, EvmChain.CHILIZ, EvmChain.CRONOS, EvmChain.FANTOM, EvmChain.HECO, EvmChain.KCC, EvmChain.OKEXCHAIN, EvmChain.OPTIMISM, EvmChain.PALM, EvmChain.XDAI, EvmChain.GNOSIS, EvmChain.zkSync];
+
+//   const response = await Moralis.EvmApi.wallets.getWalletActiveChains({
+//     address,
+//     chains,
 //   });
 
-  const address = "0x26fcbd3afebbe28d0a8684f790c48368d21665b5";
+//   console.log(response.toJSON());
+// }
 
-  const chains = [EvmChain.ETHEREUM, EvmChain.BSC, EvmChain.POLYGON , EvmChain.ARBITRUM, EvmChain.AVALANCHE, EvmChain.BASE, EvmChain.CHILIZ, EvmChain.CRONOS, EvmChain.FANTOM, EvmChain.HECO, EvmChain.KCC, EvmChain.OKEXCHAIN, EvmChain.OPTIMISM, EvmChain.PALM, EvmChain.XDAI, EvmChain.GNOSIS, EvmChain.zkSync];
-
-  const response = await Moralis.EvmApi.wallets.getWalletActiveChains({
-    address,
-    chains,
-  });
-
-  console.log(response.toJSON());
-}
-
-runApp();
+// runApp();
 
 const fetchBalanceFromEtherscan = async (address) => {
 	const url = `https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=HVHTPWF3UJ8P5ZEDNUZYMT28ZZNEEURRD4`;
