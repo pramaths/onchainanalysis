01148f479c2fbcc3277119cbc0cf7414b22c124e
pramaths <103276292+pramaths@users.noreply.github.com>
2024-07-15 13:45:14 +0530

    removed log file

--- serializers/btcSerializer.js
+++ serializers/btcSerializer.js
@@ -7,9 +7,9 @@ console.log("address",address);
       console.log("No vin found in transaction:", transaction.txid);
       const tx = {
         block_hash: transaction.status.block_hash || "",
-        block_number: transaction.status.block_no || "",
-        block_timestamp: transaction.status.block_timestamp
-          ? new Date(transaction.status.block_timestamp).toISOString()
+        block_number: transaction.status.block_time || "",
+        block_timestamp: transaction.status.block_time
+          ? new Date(transaction.status.block_time).toISOString()
           : "",
         from_address: transaction.prevout.scriptpubkey_address,
         to_address: address,
@@ -25,9 +25,9 @@ console.log("address",address);
       if (input.prevout && input.prevout.scriptpubkey_address !== address) {
         const tx = {
           block_hash: transaction.status.block_hash || "",
-          block_number: transaction.status.block_no || "",
-          block_timestamp: transaction.status.block_timestamp
-            ? new Date(transaction.status.block_timestamp).toISOString()
+          block_number: transaction.status.block_height || "",
+          block_timestamp: transaction.status.block_time
+            ? new Date(transaction.status.block_time).toISOString()
             : "",
           from_address: input.prevout.scriptpubkey_address  || "pramath",
           to_address: address ,
@@ -48,9 +48,9 @@ console.log("address",address);
     ) {
       const tx = {
         block_hash: transaction.status.block_hash || "",
-        block_number: transaction.status.block_no || "",
-        block_timestamp: transaction.status.block_timestamp
-          ? new Date(transaction.status.block_timestamp).toISOString()
+        block_number: transaction.status.block_height || "",
+        block_timestamp: transaction.status.block_time
+          ? new Date(transaction.status.block_time).toISOString()
           : "",
         from_address: address,
         to_address: output.scriptpubkey_address || "",
