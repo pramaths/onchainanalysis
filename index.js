85bfb25674c910e6235fd6c62a414e92d98891f9
pramaths <103276292+pramaths@users.noreply.github.com>
2024-07-15 13:09:15 +0530

    logger

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(compression());
 // );
 
 // Middleware setup
-app.use(morgan('tiny', { stream: logger.stream }));
+app.use(morgan('combined'));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
