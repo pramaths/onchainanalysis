c02043bdcc8e0c99030098d854b4aec13c392bdc
pramaths <103276292+pramaths@users.noreply.github.com>
2024-07-15 13:22:57 +0530

    removed log file

--- index.js
+++ index.js
@@ -41,8 +41,6 @@ app.use(compression());
 app.use(morgan('combined'));
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 30000 // Increase timeout to 30 seconds
 }).then(() => {
   console.log('MongoDB connected successfully 🚀🚀🚀');
